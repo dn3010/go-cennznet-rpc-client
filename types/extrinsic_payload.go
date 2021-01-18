404c5128b05f1a5217763163a6ac09ff70d81209
Hiruna Jayamanne <hiru@sylo.io>
2021-01-18 14:25:33 +1300

    Remove instance of code that can panic
    Encoding an extrinsic panics if the FeeExchange version is not 1.
    Change this to return an error instead.

--- types/extrinsic_payload.go
+++ types/extrinsic_payload.go
@@ -17,6 +17,7 @@
 package types
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/centrifuge/go-substrate-rpc-client/scale"
@@ -102,7 +103,7 @@ func (fe FeeExchange) Encode(encoder scale.Encoder) error {
 		return encoder.Encode(fe.AsFeeExchangeV1)
 	}
 
-	panic("Only FeeExchangeV1 is supported")
+	return errors.New("Only FeeExchangeV1 is supported")
 }
 
 func (fe *FeeExchange) Decode(decoder scale.Decoder) error {
